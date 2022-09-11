391e1469578698a22140155c24c0625df16b5935
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-09-11 17:25:49 +0200

    Main commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [{
 		name            : 'rbpi-status',
-		script          : './client/index.js',
+		script          : './index.js',
 
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
 		error_file      : '/home/ubuntu/logs/server/rbpi-status/error.log',
